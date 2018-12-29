5a1307f871a2ea8720f9542bbd7dc5f9bd99c5bc
Jae Bradley <jae.b.bradley@gmail.com>
2018-12-29 12:03:15 -0500

    Setup

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+const config = {
+  presets: [
+    '@babel/env',
+  ]
+};
+
+module.exports = config;
---
+++ eslintrc.js
@@ -0,0 +1,7 @@
+const config = {
+  extends: [
+    'airbnb',
+  ],
+};
+
+module.exports = config;
